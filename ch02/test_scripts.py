99a31be11c482e3672b2f9b6cb169b396f929844
t.onodera <t.onodera.1.2.1.5@gmail.com>
2023-11-01 09:07:46 +0900

    2章まで完了

--- ch02/test_scripts.py
+++ ch02/test_scripts.py
@@ -3,20 +3,31 @@ sys.path.append("..")
 from common import utils
 import numpy as np
 import matplotlib.pyplot as plt
+from dataset import ptb
 
-text = 'You say goodby and I say hello.'
-corpus, word_to_id, id_to_word = utils.preprocess(text)
+corpus, word_to_id, id_to_word = ptb.load_data('train')
+
+window_size = 2
+wordvec_size = 100
 vocab_size = len(word_to_id)
+print('共起行列を生成')
+C = utils.create_co_matrix(corpus, vocab_size, window_size)
+print('共起行列をから相互情報量の行列に変換')
+W = utils.ppmi(C, verbose=True)
 
-C = utils.create_co_matrix(corpus, vocab_size)
-W = utils.ppmi(C)
+print('SVD(特異値分解)による次元削減を実行')
+try:
+    from sklearn.utils import extmath
+    U, S, V = extmath.randomized_svd(W, n_components=wordvec_size, n_iter=5, random_state=None)
+except ImportError:
+    print("===Import Error===")
+    U, S, V = np.linalg.svd(W)
+    print('numpyのsvd()を実行しました')
+    
+word_vecs = U[:, :wordvec_size]
 
-np.set_printoptions(precision=3)
+querys = ['you', 'year', 'car', 'toyota']
+for query in querys:
+    utils.most_similar(query, word_to_id, id_to_word, word_vecs, top=5)
 
-U, S, V = np.linalg.svd(W)
 
-for word, word_id in word_to_id.items():
-    plt.annotate(word, (U[word_id, 0], U[word_id, 1]))
-
-plt.scatter(U[:,0], U[:,1], alpha=0.5)
-plt.show()
