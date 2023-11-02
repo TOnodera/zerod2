6e5202fb8e3a5e6be17dfb2bc4b12f1d0875a8c5
t.onodera <t.onodera.1.2.1.5@gmail.com>
2023-11-02 10:51:06 +0900

    word2vecの改良①

--- common/layers.py
+++ common/layers.py
@@ -84,3 +84,23 @@ class SoftmaxWithLoss:
         dx = dx / batch_size
 
         return dx
+
+
+class Embedding:
+    def __init__(self, W: NDArray) -> None:
+        self.W = [W]
+        self.grads = [np.zeros_like(W)]
+        self.idx = None
+        
+    def forward(self, idx):
+        W, = self.params
+        self.idx = idx
+        out = W[idx]
+        return out
+    
+    def backward(self, dout) -> None:
+        dW, = self.grads
+        dW[...] = 0
+        np.add.at(dW, self.idx, dout)
+        
+    
